f8c4c492089be0c34302585f73779fc8af6efd29
rajveer1131 <pal.ritik15@gmail.com>
2024-01-26 06:06:17 +0530

    Did some changes

--- api/index.js
+++ api/index.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
-dotenv.config();
+// dotenv.config();
+import 'dotenv/config'
 
 mongoose
   .connect(process.env.MONGO)
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,6 +5,9 @@ import { useEffect, useState } from 'react';
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
+  // const name =;
+  // const capitalizedName = ;
+
   const [searchTerm, setSearchTerm] = useState('');
   const navigate = useNavigate();
   const handleSubmit = (e) => {
@@ -12,6 +15,7 @@ export default function Header() {
     const urlParams = new URLSearchParams(window.location.search);
     urlParams.set('searchTerm', searchTerm);
     const searchQuery = urlParams.toString();
+    
     navigate(`/search?${searchQuery}`);
   };
 
@@ -22,13 +26,17 @@ export default function Header() {
       setSearchTerm(searchTermFromUrl);
     }
   }, [location.search]);
+    useEffect(() => {
+    // Clear search term after navigation
+    setSearchTerm('');
+  }, [navigate]);
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Sahand</span>
-            <span className='text-slate-700'>Estate</span>
+            <span className='text-slate-500'>Market</span>
+            <span className='text-slate-700'>Place</span>
           </h1>
         </Link>
         <form
@@ -57,13 +65,15 @@ export default function Header() {
               About
             </li>
           </Link>
+          <Link to='/create-listing'>
+            <li className='hidden sm:inline text-slate-700 hover:underline '>
+              Create
+            </li>
+          </Link>
           <Link to='/profile'>
             {currentUser ? (
-              <img
-                className='rounded-full h-7 w-7 object-cover'
-                src={currentUser.avatar}
-                alt='profile'
-              />
+              
+              <h1 className='text-blue-800 font-extrabold hover:scale-105'>{currentUser.username.charAt(0).toUpperCase() + currentUser.username.slice(1)}</h1>
             ) : (
               <li className=' text-slate-700 hover:underline'> Sign in</li>
             )}
